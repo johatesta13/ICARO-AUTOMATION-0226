ac3960d2b1896be8301da46ba23fe358abc96bbf
johatesta13 <johatesta@gmail.com>
2026-04-28 11:51:18 -0300

    cuarta clase

---
+++ CLASE 4/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+
+    void main (){
+
+
+        Estudiante estudianteMaria= new Estudiante (1001,"Maria Gonzales",9.0f);
+
+
+        System.out.println("ID: "  + estudianteMaria.getId());
+        System.out.println("ID: "  + estudianteMaria.id());
+        System.out.println("Nombre: "  + estudianteMaria.getNombreCompleto());
+        System.out.println("Promedio: "  + estudianteMaria.getPromedio());
+
+
+
+        estudianteMaria.setPromedio(9.5f);
+        System.out.println("Promedio Modificado: "  + estudianteMaria.getPromedio());
+
+
+
+    }
+
+}
